60bffbc9e011c649a2ca74502611fed3c820da4e
Daniel <dan13lstz@abv.bg>
2020-04-05 22:54:21 +0300

    Problem1 added

---
+++ Problem1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding utf8 = Encoding.UTF8;
+            StreamReader reader = new StreamReader("../../subs.txt", utf8);
+
+            using (reader)
+            {
+                int lineNumber = 0;
+                string line = reader.ReadLine();
+
+                while(line != null)
+                {
+                    if(lineNumber%2==1)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    lineNumber++;
+                    line = reader.ReadLine();
+
+                }
+            }
+        }
+    }
+}

abb144fb241a35dda2f8f7c34b58949a8b3bf169
Daniel <dan13lstz@abv.bg>
2020-04-07 22:19:49 +0300

    Problem2 added

---
+++ Problem2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("../../Conc1.txt");
+            string text1 = reader.ReadToEnd();
+
+            StreamReader reader2 = new StreamReader("../../Conc2.txt");
+            string text2= reader2.ReadToEnd();
+
+            string text3 = String.Concat(text1, text2);
+
+            StreamWriter writer3 = new StreamWriter("../../Conc3.txt");
+
+            using (writer3)
+            {
+                writer3.WriteLine(text3);
+            }
+        }
+    }
+}

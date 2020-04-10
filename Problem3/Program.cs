aabd9ff5bc0c2a3c35ba652afd9d27bf8ecf152e
Daniel <dan13lstz@abv.bg>
2020-04-10 23:49:04 +0300

    Problem3 added

---
+++ Problem3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("../../text1.txt");
+            List<string> lines = new List<string>();
+
+            using (reader)
+            {
+                int lineNumber = 1;
+                string line = reader.ReadLine();
+
+                while(line != null )
+                {
+                    lines.Add(lineNumber+ " : " + line);
+                    line = reader.ReadLine();
+                    lineNumber++;
+                }
+            }
+
+            StreamWriter writer = new StreamWriter("../../text2.txt");
+
+            using (writer)
+            {
+                foreach (string value in lines)
+                {
+                    writer.WriteLine(value);
+                }
+            }
+        }
+    }
+}
